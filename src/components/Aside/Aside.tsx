ffebc50201ed1cf1c71e053fba8d6bed8cd85468
Nikita Bystritsky <nbystritsky@gmail.com>
2025-04-25 17:42:28 +0500

    refactor: make aside component

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import Header from '../Header/Header'
 import NavigationMenu from '../NavigationMenu/NavigationMenu'
 import Questions from '../Questions/Questions'
+import { Flex } from 'antd';
 
 function Aside() {
     return (
     <>
-        <Header/>
-        <div style={{display:'flex'}}>
+        <div
+        style={{
+            width: "250px",
+            height: "72px",
+            backgroundColor: "#2E323A",
+            padding: "16px 98px 16px 32px",
+            borderBottom: "1px solid rgb(255, 255, 255, 6%)",
+        }}
+        >
+        <img src="\src\components\test\Logotip_UrFU_Napisanie.png" />
+        </div>
+        <Flex>
             <NavigationMenu/>
             <Questions/>
-        </div>
+        </Flex>
     </>
     )
 }
