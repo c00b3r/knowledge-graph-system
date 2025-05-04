488d1addb09dd699859cc580f0b91a74137a9c17
Nikita Bystritsky <nbystritsky@gmail.com>
2025-05-04 23:06:28 +0500

    feat: remake graph and full done sidebar

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,17 +1,25 @@
-import { Flex } from 'antd'
-import ArrowLeft from '../icons/ArrowLeft'
-import ArrowRight from '../icons/ArrowRight'
-import './style.css'
+import { Flex } from "antd";
+import ArrowLeft from "../icons/ArrowLeft";
+import ArrowRight from "../icons/ArrowRight";
+import "./style.css";
+import { Breadcrumb } from "antd";
 
 function MainHeader() {
   return (
-    <div className='wrapper'>
-       <Flex gap={24.6} align='center' className='arrows-wrapper'>
-            <ArrowLeft/>
-            <ArrowRight/>
-       </Flex>
+    <div className="wrapper">
+      <Flex gap={24.6} align="center" className="arrows-wrapper">
+        <ArrowLeft />
+        <ArrowRight />
+      </Flex>
+      <Breadcrumb className="breadcrumb"
+        items={[
+          {
+            title: "Home",
+          },
+        ]}
+        />
     </div>
-  )
+  );
 }
 
-export default MainHeader
+export default MainHeader;
