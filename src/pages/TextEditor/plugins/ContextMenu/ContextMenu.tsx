d2527961e1755b32a782599fa8ff5ea324dcac0f
c00b3r <sanuyOKmaklaren0@gmail.com>
2025-06-06 22:39:05 +0500

    refactor: enhance ContextMenu positioning logic and clean up CSS styles

--- src/pages/TextEditor/plugins/ContextMenu/ContextMenu.tsx
+++ src/pages/TextEditor/plugins/ContextMenu/ContextMenu.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 export default function ContextMenu({
   contextMenu,
   ref,
@@ -5,12 +7,26 @@ export default function ContextMenu({
   contextMenu: { x: number; y: number } | null;
   ref: React.RefObject<HTMLDivElement | null>;
 }) {
+  const [positionY, setPositionY] = useState(contextMenu?.y);
+
+  useEffect(() => {
+    if (!contextMenu || !positionY || !ref.current?.parentElement?.clientHeight)
+      return;
+    console.log(ref.current?.parentElement?.clientHeight, contextMenu.y);
+
+    if (contextMenu.y + 55 > ref.current?.parentElement?.clientHeight) {
+      setPositionY(contextMenu.y - 100);
+    } else {
+      setPositionY(contextMenu.y + 12);
+    }
+  }, [contextMenu, contextMenu?.y, positionY, ref]);
+
   return (
     <div
       className='context-menu'
       style={{
         left: contextMenu?.x,
-        top: contextMenu?.y && contextMenu.y + 12,
+        top: positionY,
         position: 'fixed',
         width: '100px',
         height: '100px',
