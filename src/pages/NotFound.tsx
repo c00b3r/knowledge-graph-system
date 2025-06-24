c04959fa3d667ec75ef67d99f4c2947c4931b102
Nikita Bystritsky <nbystritsky@gmail.com>
2025-06-25 02:17:01 +0500

    feat: add gif to 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 function NotFound() {
-  return <div>Мы работаем над этой страницей</div>;
+  return <><div style={{margin:'30px 0 30px 100px ', fontSize: '24px'}}>Мы работаем над этой страницей</div>
+  <img src="\gifs\aa.gif" style={{marginLeft: '100px', width: '300px'}}></img></>;
 }
 
 export default NotFound;
