3d8c9587889de7d153402412cb8842465e6b0710
c00b3r <sanuyOKmaklaren0@gmail.com>
2025-06-24 22:49:09 +0500

    feat: add JsonEditor plugin to TextEditor for JSON export functionality and standardize string quotes

--- src/pages/TextEditor/TextEditor.tsx
+++ src/pages/TextEditor/TextEditor.tsx
@@ -1,74 +1,75 @@
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
-import { HeadingNode } from "@lexical/rich-text";
-import { LinkNode } from "@lexical/link";
-import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
-import InitialStructurePlugin from "./plugins/InitialStructurePlugin";
-import "./TextEditor.css";
-import FloatingTextFormatToolbarPlugin from "./plugins/FloatingTextFormater/FloatingTextFormater";
-import { useEffect, useRef, useState } from "react";
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { EditorThemeClasses } from "lexical";
-import HeadingInput from "./ui/HeadingInput";
-import ContextMenu from "./plugins/ContextMenu/ContextMenu";
-import FloatingLinkEditorPlugin from "./plugins/FloatingLinkEditorPlugin/FloatingLinkEditorPlugin";
-import { ImageNode } from "./nodes/ImageNode";
-import ImagesPlugin from "./plugins/ImagesPlugin/ImagesPlugin";
-import DragDropPastePlugin from "./plugins/DragDropPastePlugin/DragDropPastePlugin";
-import { ListPlugin } from "@lexical/react/LexicalListPlugin";
-import { ListItemNode, ListNode } from "@lexical/list";
-import CheckListPlugin from "./plugins/CheckListPlugin/CheckListPlugin";
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
+import { HeadingNode } from '@lexical/rich-text';
+import { LinkNode } from '@lexical/link';
+import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
+import InitialStructurePlugin from './plugins/InitialStructurePlugin';
+import './TextEditor.css';
+import FloatingTextFormatToolbarPlugin from './plugins/FloatingTextFormater/FloatingTextFormater';
+import { useEffect, useRef, useState } from 'react';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { EditorThemeClasses } from 'lexical';
+import HeadingInput from './ui/HeadingInput';
+import ContextMenu from './plugins/ContextMenu/ContextMenu';
+import FloatingLinkEditorPlugin from './plugins/FloatingLinkEditorPlugin/FloatingLinkEditorPlugin';
+import { ImageNode } from './nodes/ImageNode';
+import ImagesPlugin from './plugins/ImagesPlugin/ImagesPlugin';
+import DragDropPastePlugin from './plugins/DragDropPastePlugin/DragDropPastePlugin';
+import { ListPlugin } from '@lexical/react/LexicalListPlugin';
+import { ListItemNode, ListNode } from '@lexical/list';
+import CheckListPlugin from './plugins/CheckListPlugin/CheckListPlugin';
+import JsonEditor from './plugins/JsonEditor/JsonEditor';
 
 const theme: EditorThemeClasses = {
   heading: {
-    h1: "editor-heading-h1",
-    h2: "editor-heading-h2",
-    h3: "editor-heading-h3",
+    h1: 'editor-heading-h1',
+    h2: 'editor-heading-h2',
+    h3: 'editor-heading-h3',
   },
-  paragraph: "editor-paragraph",
+  paragraph: 'editor-paragraph',
   text: {
-    bold: "editor-text-bold",
-    italic: "editor-text-italic",
-    underline: "editor-text-underline",
-    strikethrough: "editor-text-strikethrough",
-    subscript: "editor-text-subscript",
-    superscript: "editor-text-superscript",
-    code: "editor-text-code",
-    uppercase: "editor-text-uppercase",
-    lowercase: "editor-text-lowercase",
-    capitalize: "editor-text-capitalize",
+    bold: 'editor-text-bold',
+    italic: 'editor-text-italic',
+    underline: 'editor-text-underline',
+    strikethrough: 'editor-text-strikethrough',
+    subscript: 'editor-text-subscript',
+    superscript: 'editor-text-superscript',
+    code: 'editor-text-code',
+    uppercase: 'editor-text-uppercase',
+    lowercase: 'editor-text-lowercase',
+    capitalize: 'editor-text-capitalize',
   },
   list: {
-    checklist: "checklist",
-    listitem: "listItem",
-    listitemChecked: "listItemChecked",
-    listitemUnchecked: "listItemUnchecked",
+    checklist: 'checklist',
+    listitem: 'listItem',
+    listitemChecked: 'listItemChecked',
+    listitemUnchecked: 'listItemUnchecked',
     nested: {
-      listitem: "nestedListItem",
+      listitem: 'nestedListItem',
     },
   },
-  link: "editor-link",
+  link: 'editor-link',
 };
 
 const initialConfig = {
-  namespace: "MyEditor",
+  namespace: 'MyEditor',
   theme,
   nodes: [HeadingNode, LinkNode, ImageNode, ListNode, ListItemNode],
   onError: (error: Error) => console.error(error),
   editorState: null,
   formatters: [
-    "bold",
-    "italic",
-    "underline",
-    "strikethrough",
-    "subscript",
-    "superscript",
-    "code",
-    "uppercase",
-    "lowercase",
-    "capitalize",
+    'bold',
+    'italic',
+    'underline',
+    'strikethrough',
+    'subscript',
+    'superscript',
+    'code',
+    'uppercase',
+    'lowercase',
+    'capitalize',
   ],
 };
 
@@ -107,7 +108,7 @@ function TextEditor() {
 
   useEffect(() => {
     const handleClick = (event: MouseEvent) => {
-      const editor = document.querySelector(".editor");
+      const editor = document.querySelector('.editor');
       const contextMenu = contextMenuRef.current;
       if (
         editor &&
@@ -119,13 +120,14 @@ function TextEditor() {
       }
     };
 
-    document.addEventListener("mousedown", handleClick);
-    return () => document.removeEventListener("mousedown", handleClick);
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
   }, []);
 
   return (
-    <div className="text-editor-container">
+    <div className='text-editor-container'>
       <LexicalComposer initialConfig={initialConfig}>
+        <JsonEditor />
         <ListPlugin />
         <CheckListPlugin />
         <HeadingInput />
@@ -133,12 +135,12 @@ function TextEditor() {
         <RichTextPlugin
           contentEditable={
             <div
-              className="editor"
+              className='editor'
               ref={onRef}
               onClick={handleClick}
               onContextMenu={handleContextMenu}
             >
-              <ContentEditable className="text-editor-content-editable" />
+              <ContentEditable className='text-editor-content-editable' />
             </div>
           }
           placeholder={null}
